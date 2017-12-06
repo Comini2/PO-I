5c3bc297514b73a0aa366a58fdf31216b4f75176
Comini2 <joaocomini@gmail.com>
2017-12-06 17:48:48 -0200

    Simplex terminado. Adicionando iterações

--- T1/js/metodos.js
+++ T1/js/metodos.js
@@ -1,3 +1,52 @@
-function simplex(a, b, c, cr, base) {
-	
+function simplex(a, b, c, cr, base, n, nr, nfp) {
+	var jMaisNegativo, iMenorPositivo, fim = false;
+
+	while(true){
+		var maisNegativo = 0;
+		for(var i = 0; i<nfp; i++){
+			if(cr[i] < maisNegativo){
+				maisNegativo = cr[i];
+				jMaisNegativo = i;
+			}
+		}
+		if(maisNegativo == 0)
+			break;
+
+		var menorPositivo = Infinity;
+		for(var i = 0; i<nr; i++){
+			var ba = b[i]/a[i][jMaisNegativo];
+			if(ba > 0 && ba < menorPositivo){
+				menorPositivo = ba;
+				iMenorPositivo = i;
+			}
+		}
+		if(menorPositivo == Infinity)
+			break;
+
+		base[iMenorPositivo] = jMaisNegativo;
+
+		var pivo = a[iMenorPositivo][jMaisNegativo];
+
+		for(var j = 0; j<nfp; j++){
+			a[iMenorPositivo][j] /= pivo;
+		}
+		b[iMenorPositivo] /= pivo;
+
+		for(var i = 0; i<nr; i++){
+			if(i != iMenorPositivo){
+				var m = a[i][jMaisNegativo]/a[iMenorPositivo][jMaisNegativo];
+				for(var j = 0; j<nfp; j++){
+					a[i][j] -= m*a[iMenorPositivo][j];
+				}
+				b[i] -= m*b[iMenorPositivo];
+			}
+		}
+		
+		for(var j = 0; j<nfp; j++){
+			cr[j] = c[j];
+			for(var i = 0; i<nr; i++){
+				cr[j] -= c[base[i]]*a[i][j];
+			}
+		}
+	}
 }
--- T1/js/scripts.js
+++ T1/js/scripts.js
@@ -1,5 +1,112 @@
 $(document).ready(function() {
-	var a, b, c, cr, base;
+	var a = [], b = [], c = [], cr = [], base = [], n = 2, nr = 2, nfp, maiorCusto = 0;
+
+	$("#n").change(function(){
+		n = parseInt($("#n").val());
+
+		if(n < 1){
+			$("#n").val(2);
+			n = 2;
+		}else if(n > 10){
+			$("#n").val(10);
+			n = 10;
+		}
+
+		$("#funcao").empty();
+
+		$("#funcao").append("z = ");
+
+		for(i = 0; i<n; i++){
+			if(i < n-1)
+				$("#funcao").append('<input type="text" class="vals" name="f' + i + '">x<sub>' + (i+1) + '</sub> +');
+			else
+				$("#funcao").append('<input type="text" class="vals" name="f' + i + '">x<sub>' + (i+1) + '</sub>');
+		}
+	});
+
+	$("#nr").change(function(){
+		nr = parseInt($("#nr").val());
+
+		if(nr < 1){
+			$("#nr").val(2);
+			nr = 2;
+		}else if(nr > 10){
+			$("#nr").val(10);
+			nr = 10;
+		}
+
+		$("#restricoes").empty();
+
+		$("#restricoes").append("<label>Restrições: </label>");
+
+		for(var i =0; i<nr; i++){
+			var $div = $("<div></div>");
+			for(var j = 0; j<n; j++){
+				if(j < n - 1)
+					$div.append('<input type="text" class="vals" name="a'+ i + j +'">x<sub>'+ (j+1) +'</sub> +');
+				else
+					$div.append('<input type="text" class="vals" name="a' + i + j +'">x<sub>'+ (j+1) +'</sub>');
+				console.log("ue");
+			}
+
+			$div.append('<select name="s' +i+ '"><option value="le">&le;</option><option value="equal">=</option><option value="ge">&ge;</option></select>');
+			$div.append('<input type="text" class="vals" name="b'+i+'">');
+
+			$("#restricoes").append($div);
+		}
+	});
+
+	$("#calcula").click(function(){
+		
+		nfp = n;
+		nr = parseInt($("#nr").val());
+
+		var fp = $("#tipo").val() == "min" ? 1 : -1;
+
+		for(var i = 0; i<n; i++){
+			c[i] = parseFloat($("input[name=f"+i+"]").val())*fp;
+			if(Math.abs(c[i]) > maiorCusto)
+				maiorCusto = Math.abs(c[i]);
+		}
+
+		for(var i = 0; i<nr; i++){
+			a[i] = [];
+			b[i] = parseFloat($("input[name=b"+i+"]").val());
+
+			for(var j = 0; j<n; j++)
+				a[i][j] = parseFloat($("input[name=a"+i+j+"]").val());
+
+			var s = $("select[name=s"+i+"]").val();
+			if(s == "le"){
+				base[i] = nfp;
+				a[i][nfp] = 1;
+				c[nfp++] = 0;
+			}else if(s == "equal"){
+				base[i] = nfp;
+				a[i][nfp] = 1;
+				c[nfp++] = maiorCusto*10;
+			}else if(s == "ge"){
+				base[i] = nfp+1;
+				a[i][nfp] = -1;
+				c[nfp++] = 0;
+				a[i][nfp] = 1;
+				c[nfp++] = maiorCusto*10;
+			}
+		}
+
+		for(var i = 0; i<nr; i++){
+			for(var j = 0; j<nfp; j++){
+				if(a[i][j] == undefined)
+					a[i][j] = 0;
+			}
+		}
+
+		for(var i=0; i<c.length; i++)
+			cr[i] = c[i];
+
+		simplex(a, b, c, cr, base, n, nr, nfp);
+	});
+
 
 	
 });
